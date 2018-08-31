aff28799163c7abbe480b9de59ec89ed9214872d
Thiago F C Silveira <thiagofcs@gmail.com>
2018-08-31 20:57:50 -0300

    Initial commit

---
+++ financial.py
@@ -0,0 +1,19 @@
+def calc_amount(pr, mo, ir):
+    """
+    Calculates compounding interest and returns both period and amount calculated
+    after each period
+    :param pr: value for principal
+    :param mo: number of periods to walk trough
+    :param ir: value for the interest rate
+    :return: [x] list of period and [y] list of accumulated amount
+    """
+    mo += 1
+    amount = 0
+    x = list()
+    y = list()
+    for month in range(1, mo):
+        amount = amount + pr * (1.0 + ir) ** month
+        print('{:2.2f}'.format(amount))
+        x.append(month)
+        y.append(amount)
+    return x, y
